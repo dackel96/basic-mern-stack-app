ec75c7b16e14dd1600deed06111d3e3d26d8536d
Ivan Iliev <ivanilievdackel@gmail.com>
2023-08-01 08:30:53 +0300

    server configuration

---
+++ server/routes/record.mjs
@@ -0,0 +1,60 @@
+import express from 'express';
+import db from '../db/conn.mjs';
+import { ObjectId } from 'mongodb';
+
+const router = express.Router();
+// Listed records
+router.get('/', async (req, res) => {
+  let collection = await db.collection('records');
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
+});
+// Records by ID
+router.get('/:id', async (req, res) => {
+  let collection = await db.collection('records');
+  let query = { _id: new ObjectId(req.params.id) };
+  let result = await collection.findOne(query);
+
+  if (!result) res.send('Not found').status(404);
+  else res.send(result).status(200);
+});
+// Create new records
+router.post('/', async (req, res) => {
+  let newDocument = {
+    name: req.body.name,
+    position: req.body.position,
+    level: req.body.level,
+  };
+
+  let collection = await db.collection('records');
+  let result = await collection.insertOne(newDocument);
+
+  res.send(result).status(204);
+});
+//Update records
+router.patch('/:id', async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+  const updates = {
+    $set: {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level,
+    },
+  };
+
+  let collection = await db.collection('records');
+  let result = await collection.updateOne(query, updates);
+
+  res.send(result).status(200);
+});
+//Delete records
+router.delete('/:id', async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+
+  const collection = db.collection('records');
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
+});
+
+export default router;
