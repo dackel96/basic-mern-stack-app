a2cea8a42a5ef710f0f7a40baca4dea8b24ab3cc
Ivan Iliev <ivanilievdackel@gmail.com>
2023-08-06 13:26:21 +0300

    finished

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,17 @@
 /* eslint-disable no-unused-vars */
 import { React } from 'react';
-import { ReactDOM } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+const root = createRoot(document.getElementById('root'));
+
+const app = (
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
+
+root.render(app);
